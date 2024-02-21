f09eb0eda949f171b371ce17d865e272bb08f980
Krishna-13-cyber <krishnanarayanan132002@gmail.com>
2024-02-21 22:39:36 +0530

    Update convention

--- .github/action_test/entrypoint.sh
+++ .github/action_test/entrypoint.sh
@@ -9,7 +9,7 @@ echo "Going into build directory"
 cd build
 
 echo "Generating cmake files"
-cmake -DLLVM_DIR=/usr/lib/llvm-11/build/ -DCMAKE_BUILD_TYPE=DEBUG ../
+cmake -DLLVM_DIR=/usr/lib/llvm-11/build/ -DCMAKE_BUILD_TYPE=DEBUG -DLLVM_EXTERNAL_LIT="$(which lit)" ../
 
 echo "Running make"
 make -j 8
